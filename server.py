65d0adf253c0776918cbfcbe4b7863cc7e9e5655
makst <maks.tses@gmail.com>
2025-05-06 22:35:41 +0500

    Fixed project

---
+++ server.py
@@ -0,0 +1,17 @@
+﻿from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'])
+def main_page():
+    return render_template('main-page.html')
+
+
+@app.route('/scripts/<filename>', methods=['GET'])
+def get_script(filename):
+    return render_template()
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
