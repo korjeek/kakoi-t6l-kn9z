c71e880d276da4b1dbc4e3c939bd7c6c8da2538b
makst <maks.tses@gmail.com>
2025-05-06 23:11:48 +0500

    Fixed for Goga implementation

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-﻿from flask import Flask, render_template
+﻿from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -8,9 +8,15 @@ def main_page():
     return render_template('main-page.html')
 
 
-@app.route('/scripts/<filename>', methods=['GET'])
-def get_script(filename):
-    return render_template()
+@app.get('/create-test.html')
+def get_create_test_page():
+    return render_template('create-test.html')
+
+
+@app.post('/create-test.html')
+def create_test():
+    print(request.form)
+    return render_template('create-test.html')
 
 
 if __name__ == '__main__':
