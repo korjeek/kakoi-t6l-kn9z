41de55733012da06f2445602acb5deac3dddd9e2
korjeek <olegka823@gmail.com>
2025-05-06 15:22:57 +0500

    first commit

---
+++ scripts/add-quiz.js
@@ -0,0 +1,23 @@
+﻿function addQuestion() {
+    const container = document.getElementById('questionsContainer');
+    const questionId = container.children.length + 1;
+
+    const questionDiv = document.createElement('div');
+    questionDiv.className = 'question';
+    questionDiv.innerHTML = `
+        <h4>Вопрос ${questionId}</h4>
+        <input type="text" placeholder="Текст вопроса" required>
+        <div class="answers"></div>
+        <button type="button" onclick="addAnswer(this)">Добавить вариант</button>
+    `;
+    container.appendChild(questionDiv);
+}
+
+function addAnswer(button) {
+    const answersDiv = button.parentElement.querySelector('.answers');
+    const answerInput = document.createElement('input');
+    answerInput.type = 'text';
+    answerInput.placeholder = 'Вариант ответа';
+    answerInput.required = true;
+    answersDiv.appendChild(answerInput);
+}
